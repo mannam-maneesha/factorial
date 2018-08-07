ec25e84b17c642401a1771708b0034d4a80fcf8f
mannam-maneesha <41893993+mannam-maneesha@users.noreply.github.com>
2018-08-07 14:39:13 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n=1,i,fact;
+	Scanner sc=new Scanner (System.in);
+	for(i=1;i<=5;i++)
+	{
+		fact=n*i;
+	}
+	System.out.println(n*i);
+	
+	}
+}
